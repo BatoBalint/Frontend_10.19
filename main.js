dd2fdc1e02d91040c4e34385c8ee1a3f7762c816
BatoBalint <balint.bato@gmail.com>
2021-10-19 16:06:53 +0200

    btn, settimeout

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 function click() {
-  while (true) {
-    
+  setTimeout(changeColor, 1000);
+}
+
+let color = 'blue';
+
+function changeColor() {
+  let btn = document.getElementById('btn');
+  if (color === 'blue') {
+    color = 'green';
+    btn.classList.add('green');
+    btn.classList.remove('blue');
+  } else {
+    color = 'blue';
+    btn.classList.add('blue');
+    btn.classList.remove('green');
   }
 }
 
 function Init() {
-  document.getElementById('btn', katt);
+  document.getElementById('btn').addEventListener('click', click);
 }
 
 document.addEventListener('DOMContentLoaded', Init);
